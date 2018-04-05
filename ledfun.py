16bdd25505baab501e0e4c539d1be283ce8f13c6
Stanley Lio <stanleylio@gmail.com>
2018-04-05 09:33:26 -1000

    .

---
+++ ledfun.py
@@ -0,0 +1,29 @@
+# Ctrl + C to terminate
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(6,GPIO.OUT)
+
+k = 0.1
+
+while True:
+    try:
+        for i in range(3):
+            GPIO.output(5,GPIO.HIGH)
+            time.sleep(k)
+            GPIO.output(5,GPIO.LOW)
+            time.sleep(k)
+
+        for i in range(3):
+            GPIO.output(6,GPIO.HIGH)
+            time.sleep(k)
+            GPIO.output(6,GPIO.LOW)
+            time.sleep(k)
+    except KeyboardInterrupt:
+        break
+
+GPIO.cleanup(5)
+GPIO.cleanup(6)
